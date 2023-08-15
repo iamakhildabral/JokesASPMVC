8c546aca3854aa966877df2cadcf00c64892ca75
iamakhildabral <akhildabral02@gmail.com>
2023-08-15 20:10:59 +0530

    Add project files.

---
+++ JokesWebApp/Models/Jokes.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int ID { get; set; }
+        public String JokesQuestion { get; set; }
+        public String JokesAnswer { get; set; }
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
